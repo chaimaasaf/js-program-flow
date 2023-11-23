03a45927d72aedee917bef9ecb16dbf65b2b19d3
chaimaasaf <chaima.aissoui17@gmail.com>
2023-11-23 16:41:39 +0100

    array methods demo

---
+++ array_metodhs.js
@@ -0,0 +1,23 @@
+var tab = ["chaima"];
+
+// add item at the last position in tab
+console.log(tab);
+
+tab.push("aissaoui");
+
+console.log(tab);
+
+// add item at the first position in tab
+
+tab.unshift("21 yo");
+console.log(tab);
+
+// remove item at the last position from tab
+tab.pop();
+
+console.log(tab);
+
+// remove item at the first position from tab
+
+tab.shift();
+console.log(tab);
